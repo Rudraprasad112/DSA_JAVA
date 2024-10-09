12813af1261241ec723a5d1f0e5b115529808463
Rudra panchanan bhuyan <rudrabhuyan6@gmail.com>
2024-10-09 19:23:59 +0530

    add arrays

---
+++ Arrays/PrintoneToTen.java
@@ -0,0 +1,9 @@
+package Arrays;
+
+public class PrintoneToTen {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
